e6b480140e149951abc5cd07053e50e4ea0e509d
Tuananh2810 <trantuananh.28102002@gmail.com>
2023-05-19 20:27:20 +0700

    Finish Header

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -5,7 +5,7 @@ const Header = () => {
     return (
         <>
             <header className="header-top-strip py-3">
-                <div className="container-xl">
+                <div className="container-xxl">
                     <div className="row">
                         <div className="col-6">
                             <p className="text-white mb-0">
@@ -28,14 +28,14 @@ const Header = () => {
                 </div>
             </header>
             <header className="header-upper py-3">
-                <div className="container-xl">
+                <div className="container-xxl">
                     <div className="row align-items-center">
                         <div className="col-2">
                             <h3>
                                 <Link className="text-white">TA Econo.</Link>
                             </h3>
                         </div>
-                        <div className="col-5">
+                        <div className="col-5 ">
                             <div className="input-group ">
                                 <input
                                     type="text"
@@ -55,28 +55,118 @@ const Header = () => {
                         <div className="col-5">
                             <div className="header-upper-links d-flex align-item-center justify-content-between">
                                 <div>
-                                    <Link>
-                                        <img src="images/acc.jpg" alt=""></img>
-                                        <p></p>
+                                    <Link className="d-flex align-items-center gap-10 text-white">
+                                        <img
+                                            src="images/compare.svg"
+                                            alt="compare"
+                                        ></img>
+                                        <p className="mb-0">
+                                            Compare <br /> Products
+                                        </p>
                                     </Link>
                                 </div>
                                 <div>
-                                    <Link>
-                                        <img src="images/" alt=""></img>
-                                        <p></p>
+                                    <Link className="d-flex align-items-center gap-10 text-white">
+                                        <img
+                                            src="images/wishlist.svg"
+                                            alt="wislist"
+                                        ></img>
+                                        <p className="mb-0">
+                                            Favotites <br /> Wislist
+                                        </p>
                                     </Link>
                                 </div>
                                 <div>
-                                    <Link>
-                                        <img src="/public/images/" alt=""></img>
-                                        <p></p>
+                                    <Link className="d-flex align-items-center gap-10 text-white">
+                                        <img
+                                            src="images/user.svg"
+                                            alt="user"
+                                        ></img>
+                                        <p className="mb-0">
+                                            Log In <br /> MyAccount
+                                        </p>
                                     </Link>
                                 </div>
                                 <div>
-                                    <Link>
-                                        <img src="/public/images/" alt=""></img>
-                                        <p></p>
+                                    <Link className="d-flex align-items-center gap-10 text-white">
+                                        <img
+                                            src="images/cart.svg"
+                                            alt="cart"
+                                        ></img>
+
+                                        <div className="d-flex flex-column gap-10">
+                                            <span className="badge bg-white text-dark">
+                                                1
+                                            </span>
+                                            <p className="mb-0">$ 500</p>
+                                        </div>
                                     </Link>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </header>
+            <header className="header-bottom py-3">
+                <div className="container-xxl">
+                    <div className="row">
+                        <div className="col-12">
+                            <div className="menu-bottom d-flex align-items-center gap-30">
+                                <div>
+                                    <div className="dropdown">
+                                        <button
+                                            className="btn btn-secondary dropdown-toggle bg-transparent border-0 gap-15 d-flex align-items-center"
+                                            type="button"
+                                            id="dropdownMenuButton1"
+                                            data-bs-toggle="dropdown"
+                                            aria-expanded="false"
+                                        >
+                                            <img
+                                                src="images/menu.svg"
+                                                alt="menu"
+                                            ></img>
+                                            <span className="me-5 d-inline-block">
+                                                Shop categories
+                                            </span>
+                                        </button>
+                                        <ul
+                                            className="dropdown-menu"
+                                            aria-labelledby="dropdownMenuButton1"
+                                        >
+                                            <li>
+                                                <Link
+                                                    className="dropdown-item text-white"
+                                                    to=""
+                                                >
+                                                    Action
+                                                </Link>
+                                            </li>
+                                            <li>
+                                                <Link
+                                                    className="dropdown-item text-white"
+                                                    to=""
+                                                >
+                                                    Another action
+                                                </Link>
+                                            </li>
+                                            <li>
+                                                <Link
+                                                    className="dropdown-item text-white"
+                                                    to=""
+                                                >
+                                                    Something else here
+                                                </Link>
+                                            </li>
+                                        </ul>
+                                    </div>
+                                </div>
+                                <div className="menu-link">
+                                    <div className="d-flex align-items-center gap-30">
+                                        <NavLink to="/">Home</NavLink>
+                                        <NavLink to="/">Our Store</NavLink>
+                                        <NavLink to="/">Blogs</NavLink>
+                                        <NavLink to="/contact">Contact</NavLink>
+                                    </div>
                                 </div>
                             </div>
                         </div>
